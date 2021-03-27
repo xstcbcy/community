54159be421d7128f97f9bfaea39f8db813bd69f0
unknown <2986548483@qq.com>
2021-03-27 09:59:01 +0800

    P14

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## ShoeBox社区
 
 ## 资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-https://elasticsearch.cn/explore
-
+[Spring 文档](https://spring.io/guides)
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)
+[ew社区](https://elasticsearch.cn/explore)
+[Bootstrap 文档](https://v3.bootcss.com/getting-started/)
+[Github OAuth 文档](https://docs.github.com/en/developers/apps/building-oauth-apps)
 ## 工具
-https://git-scm.com/downloads
+[Git](https://git-scm.com/downloads)
+[Visual paradigm](https://www.visual-paradigm.com/)
---
+++ src/main/java/life/shoebox/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package life.shoebox.community.dto;
+
+/**
+ * @author bcy
+ * @date 2021/3/25 14:01
+ */
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
